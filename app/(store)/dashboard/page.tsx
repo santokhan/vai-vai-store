2dbb5990c7aaca69582e9428a67686dc72497b34
Santo <santokhan1999@gmail.com>
2024-01-19 17:51:34 +0600

    update UI

--- app/(store)/dashboard/page.tsx
+++ app/(store)/dashboard/page.tsx
@@ -1,6 +1,10 @@
+import DashboardImage from '@/assets/images/overview-2.png'
+import Image from 'next/image'
 
 export default function Dashboard() {
     return (
-        <div></div>
+        <div>
+            <Image src={DashboardImage} alt="Dashboard" className="w-full" />
+        </div>
     )
 }
