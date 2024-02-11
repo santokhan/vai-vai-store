2e8efe8c9aaf7690007b64c5dcd626fdba447b28
Santo <santokhan1999@gmail.com>
2024-02-11 13:22:25 +0600

    Remove not exist import, bug fix

--- app/(store)/stock/table/page.tsx
+++ app/(store)/stock/table/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-import StockTable from "@/block/add/stock/table/main";
-import { OnlyChildrenProps } from "@/utils/props-type";
 
 export default function StockTablePage() {
     return (
