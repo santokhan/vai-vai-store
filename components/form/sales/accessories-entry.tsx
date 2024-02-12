4d220cf74dd7c99a0f7c2bef06fdd7f8d1a5872a
Santo <santokhan1999@gmail.com>
2024-02-12 13:01:36 +0600

    refactor

--- components/form/sales/accessories-entry.tsx
+++ components/form/sales/accessories-entry.tsx
@@ -1,37 +1,16 @@
-'use client'
-
+'use client';
 import Button from "@/components/button/button";
-import SearchProductCard from "@/components/card/search-product-card";
 import FormContainer from "@/components/form-container";
-import ReactQueryContext from "@/context/react-query-context";
-import { Customer, InStock, SalesEntry, Seller } from "@/prisma/generated/client";
-import { InitialSalesEntry, initialCustomer } from "@/utils/default-data";
-import { ORIGIN } from "@/utils/origin";
+import { InStock } from "@/prisma/generated/client";
 import { Add } from "iconsax-react";
 import { ChangeEvent, useRef, useState } from "react";
-import { useQuery } from "react-query";
 import SearchByModel from "../search/search-by-model";
 import FormTitle from "../title";
 import CloseForm from "../close-form";
 
-export type SalesEntryType = typeof InitialSalesEntry;
-export type CustomerData = typeof initialCustomer;
-export type PostSalesData = {
-    customer: CustomerData
-    discount: number,
-    due: number,
-    IMEI: string,
-    instockId: string,
-    price: number,
-    sellerId: string,
-};
-
 export default function AccessoriesSalesEntryForm({ onCloseForm }: { onCloseForm: () => void }) {
     const [adding, setAdding] = useState(false);
-    const [salesData, setsalesData] = useState<SalesEntryType>(InitialSalesEntry);
     const [foundStockItem, setfoundStockItem] = useState<InStock | null>(null);
-    const [addedSales, setaddedSales] = useState<SalesEntry | null>(null);
-    const [customer, setcustomer] = useState<CustomerData>(initialCustomer);
     const searchInputRef = useRef<HTMLInputElement | null>(null);
 
     function setSearchStockData(data: InStock) {
@@ -39,60 +18,11 @@ export default function AccessoriesSalesEntryForm({ onCloseForm }: { onCloseForm
         setfoundStockItem(data)
     }
 
-    /**
-     * setState({ ...state, [key]: value})
-     * 
-     * @param key 
-     * @param value 
-     */
-    function setCustomerData(key: string, value: string) {
-        setcustomer({ ...customer, [key]: value })
-    }
-
     async function handleSubmit(e: ChangeEvent<HTMLFormElement>) {
         e.preventDefault();
-        const { discount, sellerId } = salesData;
-        if (!foundStockItem?.id) {
-            alert('Please search product first');
-            searchInputRef.current?.focus()
-        } else if (foundStockItem?.id && customer) {
-            setAdding(true);
-            // TODO: add sales entry
-            const API_URL = `${ORIGIN}/api/sales/entry/`
-
-            /**
-             * Required data to add sales entrys
-             */
-            const body: PostSalesData = {
-                discount,
-                sellerId,
-                IMEI: foundStockItem.IMEI,
-                instockId: foundStockItem.id,
-                price: foundStockItem.price,
-                due: salesData.due,
-                customer,
-            }
-
-            await fetch(API_URL, {
-                headers: { 'Content-Type': 'application/json' },
-                method: 'POST',
-                body: JSON.stringify(body)
-            }).then(res => res.json()).then((data: SalesEntry) => {
-                setAdding(false);
-                setaddedSales(data);
-            }).catch(err => {
-                console.error(err);
-            })
-            setAdding(false);
-        }
-        // onCloseForm()
+        setAdding(true);
+        // Add data to sales entity
     }
-
-    const sellerQuery = useQuery('getAllCustomer', () =>
-        fetch(`${ORIGIN}/api/add/seller/`, {
-            cache: 'no-store'
-        }).then(res => res.json()).then((data: Customer[]) => data)
-    )
 
     return (
         <FormContainer>
--- components/form/sales/button-entry.tsx
+++ components/form/sales/button-entry.tsx
@@ -1,37 +1,18 @@
-'use client'
-
+'use client';
 import Button from "@/components/button/button";
 import FormContainer from "@/components/form-container";
-import ReactQueryContext from "@/context/react-query-context";
-import { Customer, InStock, SalesEntry, Seller } from "@/prisma/generated/client";
-import { InitialSalesEntry, initialCustomer } from "@/utils/default-data";
+import { Customer, InStock } from "@/prisma/generated/client";
 import { ORIGIN } from "@/utils/origin";
 import { Add } from "iconsax-react";
 import { ChangeEvent, useRef, useState } from "react";
 import { useQuery } from "react-query";
 import SearchByModel from "../search/search-by-model";
-import { useSalesRowContext } from "@/context/sales-context";
 import FormTitle from "../title";
 import CloseForm from "../close-form";
 
-export type SalesEntryType = typeof InitialSalesEntry;
-export type CustomerData = typeof initialCustomer;
-export type PostSalesData = {
-    customer: CustomerData
-    discount: number,
-    due: number,
-    IMEI: string,
-    instockId: string,
-    price: number,
-    sellerId: string,
-};
-
 export default function ButtonSalesEntryForm({ onCloseForm }: { onCloseForm: () => void }) {
     const [adding, setAdding] = useState(false);
-    const [salesData, setsalesData] = useState<SalesEntryType>(InitialSalesEntry);
     const [foundStockItem, setfoundStockItem] = useState<InStock | null>(null);
-    const [addedSales, setaddedSales] = useState<SalesEntry | null>(null);
-    const [customer, setcustomer] = useState<CustomerData>(initialCustomer);
     const searchInputRef = useRef<HTMLInputElement | null>(null);
     // const { salesEntity, addToSales } = useSalesRowContext();
 
@@ -40,53 +21,9 @@ export default function ButtonSalesEntryForm({ onCloseForm }: { onCloseForm: () 
         setfoundStockItem(data)
     }
 
-    /**
-     * setState({ ...state, [key]: value})
-     * 
-     * @param key 
-     * @param value 
-     */
-    function setCustomerData(key: string, value: string) {
-        setcustomer({ ...customer, [key]: value })
-    }
-
     async function handleSubmit(e: ChangeEvent<HTMLFormElement>) {
         e.preventDefault();
-        const { discount, sellerId } = salesData;
-        if (!foundStockItem?.id) {
-            alert('Please search product first');
-            searchInputRef.current?.focus()
-        } else if (foundStockItem?.id && customer) {
-            setAdding(true);
-            // TODO: add sales entry
-            const API_URL = `${ORIGIN}/api/sales/entry/`
-
-            /**
-             * Required data to add sales entrys
-             */
-            const body: PostSalesData = {
-                discount,
-                sellerId,
-                IMEI: foundStockItem.IMEI,
-                instockId: foundStockItem.id,
-                price: foundStockItem.price,
-                due: salesData.due,
-                customer,
-            }
-
-            await fetch(API_URL, {
-                headers: { 'Content-Type': 'application/json' },
-                method: 'POST',
-                body: JSON.stringify(body)
-            }).then(res => res.json()).then((data: SalesEntry) => {
-                setAdding(false);
-                setaddedSales(data);
-            }).catch(err => {
-                console.error(err);
-            })
-            setAdding(false);
-        }
-        // onCloseForm()
+        // add to sale entity
     }
 
     const sellerQuery = useQuery('getAllCustomer', () =>
