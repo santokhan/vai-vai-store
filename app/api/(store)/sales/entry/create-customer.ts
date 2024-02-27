ac050ce3ac49bcda0782b9b6ea94d63ea04232eb
Santo Khan <santokhan1999@gmail.com>
2024-02-27 17:04:26 +0600

    remove api

--- app/api/(store)/sales/entry/create-customer.ts
+++ app/api/(store)/sales/entry/create-customer.ts
@@ -1,35 +1,5 @@
-import { prisma } from "@/lib/prisma";
-
 export type APICustomerData = {
     name: string;
     email: string;
     phone: string;
-}
-
-export async function addCustomer({ name, email, phone }: APICustomerData) {
-    try {
-        const existingCustomer = await prisma.customer.findFirst({
-            where: {
-                phone: phone
-            }
-        })
-
-        if (existingCustomer) {
-            return existingCustomer;
-        } else {
-            const createdModel = await prisma.customer.create({
-                data: {
-                    name,
-                    email,
-                    phone,
-                }
-            });
-            return createdModel;
-        }
-    } catch (error) {
-        console.error('Error creating model:', error);
-    } finally {
-        // Close the Prisma Client connection
-        await prisma.$disconnect();
-    }
 }
--- app/api/(store)/sales/entry/route.ts
+++ app/api/(store)/sales/entry/route.ts
@@ -1,31 +1,4 @@
-import { prisma } from '@/lib/prisma';
 import getSalesEntry from './read';
-import { APISalesEntity, APISalesEntry } from './type';
-
-async function addSales({ customerId, salesEntity: entity, sellerId, discount, due }: {
-    customerId: string
-    due: number
-    discount: number
-    sellerId: string
-    salesEntity: APISalesEntity[]
-}) {
-    try {
-        return await prisma.salesEntry.create({
-            data: {
-                customerId,
-                discount,
-                sellerId,
-                due,
-                entity
-            }
-        })
-    } catch (error) {
-        console.error('Error creating model:', error);
-    } finally {
-        // Close the Prisma Client connection
-        await prisma.$disconnect();
-    }
-}
 
 export async function GET(): Promise<Response> {
     const data = await getSalesEntry();
