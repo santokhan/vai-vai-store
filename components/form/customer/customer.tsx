6de8129ec12e412e1d35208e1801df7a6e4f4ee9
Santo Khan <santokhan1999@gmail.com>
2024-03-02 10:32:01 +0600

    update customer form

--- components/form/customer/customer.tsx
+++ components/form/customer/customer.tsx
@@ -10,21 +10,18 @@ const CustomerForm = () => {
     return (
         <FormContainer>
             <FormTitle>Customer Information</FormTitle>
-            <div className='w-full space-y-6'>
-                <div className="flex flex-wrap lg:flex-nowrap gap-4">
-                    <InputBox>
-                        <label htmlFor="name" className="default">Name</label>
-                        <input
-                            type="text"
-                            id="name"
-                            className="default"
-                            placeholder="Santo"
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => { setCustomerData('name', e.target.value) }}
-                        />
-                    </InputBox>
-                </div>
-                <div className="flex flex-wrap lg:flex-nowrap gap-4">
-                    <InputBox>
+            <div className='w-full gap-4 grid grid-cols-1 sm:grid-cols-2'>
+                <InputBox>
+                    <label htmlFor="name" className="default">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        className="default"
+                        placeholder="Santo"
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => { setCustomerData('name', e.target.value) }}
+                    />
+                </InputBox>
+                {/* <InputBox>
                         <label htmlFor="email" className="default">Email</label>
                         <input
                             type="email"
@@ -33,29 +30,18 @@ const CustomerForm = () => {
                             placeholder="direct.santo@gmail.com"
                             onChange={(e: ChangeEvent<HTMLInputElement>) => { setCustomerData('email', e.target.value) }}
                         />
-                    </InputBox>
-                    <InputBox>
-                        <label htmlFor="phone" className="default">Phone</label>
-                        <input
-                            type="text"
-                            id="phone"
-                            className="default"
-                            placeholder="01307 230 077"
-                            required={true}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => { setCustomerData('phone', e.target.value) }}
-                        />
-                    </InputBox>
-                </div>
-                {/* <div>
-                    <label htmlFor="message" className="default">Note</label>
-                    <textarea
-                        id="message"
-                        rows={5}
+                    </InputBox> */}
+                <InputBox>
+                    <label htmlFor="phone" className="default">Phone</label>
+                    <input
+                        type="text"
+                        id="phone"
                         className="default"
-                        placeholder="Write your thoughts here..."
-                        onChange={(e: ChangeEvent<HTMLTextAreaElement>) => { setCustomerData('message', e.target.value) }}
-                    ></textarea>
-                </div> */}
+                        placeholder="01307 230 077"
+                        required={true}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => { setCustomerData('phone', e.target.value) }}
+                    />
+                </InputBox>
             </div>
         </FormContainer>
     );
