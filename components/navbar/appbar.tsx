f40a2d967429a02698baf807b470a6b85564b191
Santo Khan <santokhan1999@gmail.com>
2024-03-02 10:52:27 +0600

    update appbar

--- components/navbar/appbar.tsx
+++ components/navbar/appbar.tsx
@@ -4,6 +4,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Logo from '../logo/logo';
+import Link from 'next/link';
 
 export default function MenuAppBar() {
     return (
@@ -11,6 +12,7 @@ export default function MenuAppBar() {
             <AppBar position="static">
                 <Toolbar>
                     <Logo />
+                    <Link href="/dashboard" className="font-semibold whitespace-nowrap rounded-lg px-3 py-2 bg-gray-100 hover:bg-gray-200 text-gray-800">ড্যাশবোর্ডে যান</Link>
                     {/* <AccountMenu /> */}
                 </Toolbar>
             </AppBar>
