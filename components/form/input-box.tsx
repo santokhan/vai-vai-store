a8318a4985fb522d6f1c452322fc2416aaaa2695
Santo Khan <santokhan1999@gmail.com>
2024-03-02 10:34:53 +0600

    html label component

--- components/form/input-box.tsx
+++ components/form/input-box.tsx
@@ -3,11 +3,14 @@ import React, { ReactNode } from 'react';
 interface ContainerProps {
     children: ReactNode; // ReactNode type accepts any JSX expression as children
     className?: string; // optional className
+    labelName?: string; // optional className
+    htmlFor?: string; // optional className
 }
 
-const InputBox: React.FC<ContainerProps> = ({ children, className }) => {
+const InputBox: React.FC<ContainerProps> = ({ children, className, labelName, htmlFor }) => {
     return (
         <div className={`w-full ${className}`}>
+            {labelName && htmlFor && <label htmlFor={htmlFor} className="default">{labelName}</label>}
             {children}
         </div>
     );
