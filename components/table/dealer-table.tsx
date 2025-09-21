e6c265dbfabca3ff580611b90563ba0da1af4aee
Santo Khan <santokhan1999@gmail.com>
2025-09-21 22:01:20 +0600

    Update dealer table

--- components/table/dealer-table.tsx
+++ components/table/dealer-table.tsx
@@ -1,27 +1,33 @@
-'use client';
-
-import { Seller } from '@/prisma/generated/client';
+import { Dealer } from '@/prisma/generated/client';
 import { Table } from 'flowbite-react';
 
-export function DealerTable({ types }: { types: Seller[] }) {
-    return (
-        <div className="overflow-x-auto bg-white rounded-lg max-w-xl">
-            <Table>
-                <Table.Head>
-                    <Table.HeadCell>Dealers</Table.HeadCell>
-                </Table.Head>
-                <Table.Body className="divide-y">
-                    {
-                        types.map((type: Seller) => (
-                            <Table.Row className="bg-white" key={type.id}>
-                                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 capitalize">
-                                    {type.name}
-                                </Table.Cell>
-                            </Table.Row>
-                        ))
-                    }
-                </Table.Body>
-            </Table>
-        </div>
-    )
+interface DealerTableProps {
+  types: Dealer[]; // Make sure this matches your Dealer type
+}
+
+export function DealerTable({ types }: DealerTableProps) {
+  return (
+    <div className="overflow-x-auto bg-white rounded-lg max-w-full">
+      <Table>
+        <Table.Head>
+          <Table.HeadCell>Dealer Name</Table.HeadCell>
+          <Table.HeadCell>Description</Table.HeadCell>
+          <Table.HeadCell>Phone Number</Table.HeadCell>
+          <Table.HeadCell>Location</Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          {types.map((dealer: Dealer) => (
+            <Table.Row className="bg-white" key={dealer.id}>
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 capitalize">
+                {dealer.name}
+              </Table.Cell>
+              <Table.Cell>{dealer.description || '-'}</Table.Cell>
+              <Table.Cell>{dealer.phoneNumber || '-'}</Table.Cell>
+              <Table.Cell>{dealer.location || '-'}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </div>
+  );
 }
