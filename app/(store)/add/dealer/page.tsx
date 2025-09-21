0ffdd0546c7dab9d275accce20dfa3a1e738d29f
Santo Khan <santokhan1999@gmail.com>
2025-09-21 22:09:05 +0600

    Add revalidate

--- app/(store)/add/dealer/page.tsx
+++ app/(store)/add/dealer/page.tsx
@@ -1,12 +1,14 @@
 import { getDealers } from '@/actions/dealer'
 import DealerFormWithTable from '@/block/add/dealer'
 
+export const revalidate = 0
+
 export default async function TypePage () {
   try {
     const dealers = await getDealers()
-    
+
     return <DealerFormWithTable dealers={dealers} />
   } catch (error) {
-    console.error('Error fetching dealers:', error); 
+    console.error('Error fetching dealers:', error)
   }
 }
