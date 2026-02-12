779d6302b908910cebd9ca5727dc3b0ee9b92a0e
Santo Khan <santokhan1999@gmail.com>
2026-02-12 10:17:48 +0600

    Update invoice 2

--- actions/sales/get.ts
+++ actions/sales/get.ts
@@ -27,6 +27,58 @@ export async function getSalesIndividual(id: string): Promise<SalesInclude_C_S |
     }
 }
 
+export async function getSalesIndividualIncludeProducts(id: string): Promise<SalesInclude_C_S | null | undefined> {
+    try {
+        const sale = await prisma.salesEntry.findFirst({
+            where: {
+                id
+            },
+            include: {
+                customer: true,
+                seller: true
+            }
+        });
+
+        let entity = sale?.entity || []
+
+        if (!Array.isArray(entity)) {
+            return sale;
+        }
+
+        const data = JSON.parse(JSON.stringify(sale))
+
+        entity = await Promise.all(entity?.map(async (item: any) => {
+            const stock = await prisma.stockAndroid.findFirst({
+                where: {
+                    id: item.stockId
+                },
+                include: {
+                    productType: true,
+                    brand: true,
+                    model: true
+                }
+            })
+
+            item.brandName = stock?.brand?.brandName
+            item.model = stock?.model?.model
+            item.ram = stock?.ram
+            item.rom = stock?.rom
+            item.IMEI = stock?.IMEI
+            item.modelId = stock?.modelId
+
+            return item
+        }))
+
+        data.entity = entity
+        return data
+    } catch (error) {
+        console.error('Error creating model:', error);
+    } finally {
+        // Close the Prisma Client connection
+        await prisma.$disconnect();
+    }
+}
+
 export async function getSalesMany(): Promise<SalesInclude_C_S[] | undefined> {
     try {
         const salesData: (SalesEntry & { customer: Customer; seller: Seller })[] = await prisma.salesEntry.findMany({
--- app/(store)/sales/entry/invoice/[salesId]/v2/page.tsx
+++ app/(store)/sales/entry/invoice/[salesId]/v2/page.tsx
@@ -1,12 +1,6 @@
-'use server';
-
-import { SalesInclude_C_S, getSalesIndividual } from "@/actions/sales/get";
-import { getAccessoriesById } from "@/actions/stock/accessories/get";
-import { getStockAndroidById } from "@/actions/stock/android";
-import { getStockButtonById } from "@/actions/stock/button/get";
+import { SalesInclude_C_S, getSalesIndividualIncludeProducts } from "@/actions/sales/get";
 import Logo from "@/components/logo/logo";
 import PrintWrapper from "@/components/print-wrapper";
-import { StockAccessories } from "@/prisma/generated/client";
 import { phoneNumbers } from "@/utils/company-details";
 import Link from "next/link";
 import { twMerge } from "tailwind-merge";
@@ -32,93 +26,101 @@ const InvoiceFooter = () => (
     </>
 )
 
-const SummaryTable = async ({ entity, due, discount }: SalesInclude_C_S) => {
-    let totalPrice: number = 0;
-    if (Array.isArray(entity)) {
-        totalPrice = entity.reduce((prev: number, crnt: any) => {
-            if (crnt.price) {
-                return prev + crnt.price * (crnt.quantity || 1);
-            }
-            return prev;
-        }, 0);
-    }
+interface TableRow {
+    brandName: string,
+    model: string,
+    IMEI: string[],
+    total: number,
+    quantity: number,
+    price: number,
+    modelId: string,
+    ram: string,
+    rom: string,
+}
 
-    const paidAmount: number = totalPrice - due - (discount || 0);
-
+// const entity = {
+//     stockId: '6983274ece42cf18d734bd04',
+//     quantity: 1,
+//     price: 823,
+//     type: 'android'
+// }
+const SummaryTable = async ({ entity, due, discount, ...rest }: SalesInclude_C_S) => {
     if (!Array.isArray(entity)) return null;
 
-    const grouped: Record<string, any> = {}
-    const entries: StockAccessories[] = []
-    await Promise.all(entity.map(async ({ type, quantity, price, stockId }: any, i: number) => {
-        const getFunction = functionObject[type as keyof typeof functionObject];
-        const stockData: any = await getFunction(stockId); // NOTE: removed async await for simplicity
+    let totalPrice: number = 0;
+    const table: TableRow[] = []
 
-        if (stockData) { entries.push(stockData) }
-
-        const exisiting = grouped[stockData.modelId]
-        if (exisiting && stockData.ram == exisiting.ram && stockData.rom == exisiting.rom) {
-            exisiting.quantity += quantity || 1
-            exisiting.price = price
-            exisiting.IMEI = exisiting.IMEI?.split(',').concat(stockData.IMEI).join(', ')
-        } else {
-            stockData.quantity = stockData.quantity || 1
-            stockData.price = price
-            grouped[stockData.modelId] = stockData
+    entity?.forEach(({ quantity, price, IMEI, brandName, model, modelId, ram, rom }: any) => {
+        const row: TableRow = {
+            brandName: brandName,
+            model: model,
+            IMEI: [IMEI],
+            price: price,
+            quantity: quantity || 1,
+            total: price * (quantity || 1),
+            modelId: modelId,
+            ram: ram,
+            rom: rom,
         }
-        return stockData;
-    }))
+
+        const duplicate = table.find((row: any) => row.modelId == modelId && row.ram == ram, row.rom == rom)
+
+        if (duplicate) {
+            row.quantity += quantity || 1
+            row.total += row.quantity * row.price
+            row.IMEI = [...row.IMEI, IMEI]
+        } else {
+            table.push(row)
+        }
+    })
+
+    const paidAmount: number = totalPrice - due - (discount || 0);
 
     return (
         <div className="rounded-lg overflow-hidden mt-6">
             <table className="w-full">
                 <thead className="bg-gray-100 text-start text-sm font-semibold uppercase">
                     <tr className="whitespace-nowrap">
-                        <th className="p-2 text-gray-700 w-2/12">Brand & Model</th>
-                        <th className="p-2 text-gray-700 w-2/12">IMEI</th>
+                        <th className="p-2 text-gray-700 w-2/12">Brand, Model</th>
                         <th className="p-2 text-gray-700 w-1/12">Quantity</th>
-                        <th className="p-2 text-gray-700 w-1/12">RAM/ROM</th>
                         <th className="p-2 text-gray-700 w-1/12">Price</th>
                         <th className="p-2 text-gray-700 w-1/12">Total Price</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {Object.values(grouped).map(async (stockData: any, i: number) => {
-                        if (!stockData) return null;
-
-                        const rowTotal = stockData.quantity * stockData.price || 0;
-
+                    {table.map(async (item: any, i: number) => {
                         return (
+                            item &&
                             <tr key={i}>
-                                <td className="default capitalize whitespace-nowrap">
-                                    {stockData.brand?.brandName} {stockData.model?.model}
+                                <td className="default">
+                                    <div className="capitalize whitespace-nowrap">
+                                        <strong>{item?.brandName} {item?.model}</strong> - {item.ram}/{item.rom}
+                                    </div>
+                                    <div className="mt-2">{item.IMEI}</div>
                                 </td>
-                                <td className="default whitespace-nowrap">{stockData.IMEI}</td>
-                                <td className="default text-end">{stockData.quantity}</td>
-                                <td className="default text-end whitespace-nowrap">
-                                    {stockData.ram}GB / {stockData.rom}GB
-                                </td>
-                                <td className="default text-end whitespace-nowrap">{stockData.price}</td>
-                                <td className="default text-end whitespace-nowrap">{rowTotal}</td>
+                                <td className="default text-end">{item.quantity}</td>
+                                <td className="default text-end whitespace-nowrap">{item.price}</td>
+                                <td className="default text-end whitespace-nowrap">{item.total}</td>
                             </tr>
                         )
                     })}
                     <tr className="text-end font-semibold">
-                        <td colSpan={4}></td>
+                        <td colSpan={2}></td>
                         <td className="default whitespace-nowrap">Total</td>
                         <td className="default">{totalPrice}</td>
                     </tr>
                     <tr className="text-end font-semibold">
-                        <td colSpan={4}></td>
+                        <td colSpan={2}></td>
                         <td className="default whitespace-nowrap">Due</td>
                         <td className="default">{due}</td>
                     </tr>
                     <tr className="text-end font-semibold">
-                        <td colSpan={4}></td>
+                        <td colSpan={2}></td>
                         <td className="default whitespace-nowrap">Discount</td>
                         <td className="default">{discount || 0}</td>
                     </tr>
                     <tr className="text-end font-semibold">
-                        <td colSpan={4}></td>
+                        <td colSpan={2}></td>
                         <td className="default whitespace-nowrap">Amount Paid</td>
                         <td className="default">{paidAmount}</td>
                     </tr>
@@ -128,18 +130,15 @@ const SummaryTable = async ({ entity, due, discount }: SalesInclude_C_S) => {
     )
 }
 
-const functionObject = {
-    android: getStockAndroidById,
-    button: getStockButtonById,
-    accessories: getAccessoriesById,
+interface Props {
+    params: { salesId: string }
 }
 
-export default async function InvoicePage({ params }: { params: { salesId: string } }) {
-    const salesEntry = await getSalesIndividual(params.salesId);
+export default async function InvoicePage({ params }: Props) {
+    const salesId = params.salesId
+    const salesEntry = await getSalesIndividualIncludeProducts(salesId);
 
-    if (!salesEntry?.id) {
-        return true;
-    }
+    if (!salesEntry?.id) { return true; }
 
     return (
         <>
